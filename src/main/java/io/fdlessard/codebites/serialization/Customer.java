28a688b43187db3f78c83596c5a4acf063744a5a
François-David Lessard <fdlessard+gh@gmail.com>
2018-09-26 15:40:36 -0400

    Update Customer.java

--- src/main/java/io/fdlessard/codebites/serialization/Customer.java
+++ src/main/java/io/fdlessard/codebites/serialization/Customer.java
@@ -1,5 +1,8 @@
 package io.fdlessard.codebites.serialization;
 
+package io.fdlessard.codebites.serialization;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
@@ -22,11 +25,15 @@ public class Customer {
     private Integer integerId;
 
     @JsonSerialize(using = PropertyToSingletonListSerializer.class)
+    @JsonDeserialize(using = SingletonListToPropertyDeserializer.class)
     private String codeString;
+
     @JsonSerialize(using = PropertyToSingletonListSerializer.class)
+    @JsonDeserialize(using = SingletonListToPropertyDeserializer.class)
     private Integer codeInteger;
 
     @JsonSerialize(using = ObjectToSingletonListSerializer.class)
+    @JsonDeserialize(using = SingletonListToObjectDeserializer.class)
     private Names names;
 
 
@@ -38,4 +45,3 @@ public class Customer {
         private String lastName;
     }
 }
-
