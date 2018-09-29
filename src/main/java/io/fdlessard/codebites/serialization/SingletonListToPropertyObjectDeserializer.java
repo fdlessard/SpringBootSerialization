80173e65985abb33feada9d2924c2c49960ed6de
fdlessard <fdlessard@gmail.com>
2018-09-29 16:58:26 -0400

    More cleanup

---
+++ src/main/java/io/fdlessard/codebites/serialization/SingletonListToPropertyObjectDeserializer.java
@@ -0,0 +1,32 @@
+package io.fdlessard.codebites.serialization;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+
+import java.io.IOException;
+
+public class SingletonListToPropertyObjectDeserializer<T> extends JsonDeserializer<T> implements ContextualDeserializer {
+
+    private Class<T> typeParameterClass;
+
+    @Override
+    public JsonDeserializer<T> createContextual(DeserializationContext deserializationContext, BeanProperty beanProperty) {
+        typeParameterClass = (Class<T>) beanProperty.getType().getRawClass();
+        return this;
+    }
+
+    @Override
+    public T deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+
+        ObjectCodec objectCodec = jsonParser.getCodec();
+        JsonNode node = objectCodec.readTree(jsonParser);
+
+        return objectCodec.treeToValue(node.get(0), typeParameterClass);
+    }
+
+}
